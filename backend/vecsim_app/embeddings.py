3ccf940071e1f7609a46a8d403ebf3c5b8076672
Tyler Hutcherson <tyler.hutcherson@redis.com>
2022-10-05 10:44:30 -0400

    update routes and clean up classes

--- backend/vecsim_app/embeddings.py
+++ backend/vecsim_app/embeddings.py
@@ -3,45 +3,42 @@ import string
 
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
 
-# String cleaner
-def clean_description(description: str):
-    if not description:
-        return ""
-    # remove unicode characters
-    description = description.encode('ascii', 'ignore').decode()
+class Embeddings:
+    model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
 
-    # remove punctuation
-    description = re.sub('[%s]' % re.escape(string.punctuation), ' ', description)
+    # String cleaner
+    @staticmethod
+    def clean_description(description: str) -> str:
+        if not description:
+            return ""
+        # remove unicode characters
+        description = description.encode('ascii', 'ignore').decode()
 
-    # clean up the spacing
-    description = re.sub('\s{2,}', " ", description)
+        # remove punctuation
+        description = re.sub('[%s]' % re.escape(string.punctuation), ' ', description)
 
-    # remove urls
-    #description = re.sub("https*\S+", " ", description)
+        # clean up the spacing
+        description = re.sub('\s{2,}', " ", description)
 
-    # remove newlines
-    description = description.replace("\n", " ")
+        # remove newlines
+        description = description.replace("\n", " ")
 
-    # remove all numbers
-    #description = re.sub('\w*\d+\w*', '', description)
+        # split on capitalized words
+        description = " ".join(re.split('(?=[A-Z])', description))
 
-    # split on capitalized words
-    description = " ".join(re.split('(?=[A-Z])', description))
+        # clean up the spacing again
+        description = re.sub('\s{2,}', " ", description)
 
-    # clean up the spacing again
-    description = re.sub('\s{2,}', " ", description)
+        # make all words lowercase
+        description = description.lower()
 
-    # make all words lowercase
-    description = description.lower()
-
-    return description
+        return description
 
 
-def make(sentences: list):
-    if isinstance(sentences, list):
-        sentences = [clean_description(description) for description in sentences]
-    else:
-        sentences = clean_description(sentences)
-    return model.encode(sentences, normalize_embeddings=True)
+    def make(self, sentences: list):
+        if isinstance(sentences, list):
+            sentences = [self.clean_description(description) for description in sentences]
+        else:
+            sentences = self.clean_description(sentences)
+        return self.model.encode(sentences, normalize_embeddings=True)
