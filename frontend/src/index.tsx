ec986b6c2343f796f28d2fb1147f708071cc435e
liram11 <liram07@gmail.com>
2023-02-19 17:13:37 +0700

    configure maps and sets with immer

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,7 +5,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { enableMapSet } from "immer"
 
+enableMapSet()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
