6b19a48deef844fa0aa3810b3e0bff6b5df770cd
liram11 <liram07@gmail.com>
2023-02-19 19:11:16 +0700

    SuggestedCategories fixed

--- frontend/src/components/SuggestedCategories/index.tsx
+++ frontend/src/components/SuggestedCategories/index.tsx
@@ -1,16 +1,18 @@
 import { Link } from "@mui/material"
+import { Root } from './styles'
 import { CATEGORY_HUMAN_NAMES } from '../../constants/search_filter';
 
-export const SuggestedCategories = ({options, onClick}: {options: string[], onClick: () => void}) => {
+export const SuggestedCategories = ({ options, onClick }: { options: string[], onClick: () => void }) => {
   if (options.length === 0) return <></>
 
   return (
-    <div className="pt-2">
-      <a style={{backgroundColor: "#FFD801"}}>We found suggested categories for your query:</a> <br /> {options.map(slug =>
+    <Root>
+      <b>We found suggested categories for your query:</b> <br />
+      {['asd', 'asdasd'].map(slug =>
         <div>{slug} ({CATEGORY_HUMAN_NAMES[slug as keyof typeof CATEGORY_HUMAN_NAMES]})</div>
       )} <br />
-      <Link component="button" underline="hover" onClick={onClick}>Apply them</Link>
-    </div>
+      <Link component="button" underline="hover" onClick={onClick}>Click here to pply them</Link>
+    </Root>
   )
 }
 
