220b7d38a64bf4dabfb9867b9bde7fd16c8d57d7
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2023-02-26 19:10:42 +0100

    Untitled1 enable local backend development (#3)

---
+++ install-local.sh
@@ -0,0 +1,15 @@
+set -eux
+
+cd ./frontend
+yarn install --no-optional
+yarn build
+
+python -m pip install --upgrade pip setuptools wheel
+
+cd ../backend
+pip install -e .
+rm -rf vecsim_app/templates/build
+cp -r ../frontend/build vecsim_app/templates/
+
+cd ./vecsim_app
+chmod +x entrypoint.sh
