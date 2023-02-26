f314840ace874627faab20779a743573d3553a14
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2023-02-26 18:47:04 +0100

    Wait for local redis

--- run-local.sh
+++ run-local.sh
@@ -2,7 +2,7 @@ set -eux
 
 pwd0=$(pwd)  # initial working dir
 DOCKER_COMPOSE=${DOCKER_COMPOSE:-docker compose}  # on some Mac machines, it's docker-compose
-$DOCKER_COMPOSE -f docker-local-redis.yml up -d redis-vector-db
+$DOCKER_COMPOSE -f docker-local-redis.yml up --detach --wait redis-vector-db
 function stop() { $DOCKER_COMPOSE -f $pwd0/docker-local-redis.yml down ;}
 trap stop EXIT  # stop containers when finish
 
