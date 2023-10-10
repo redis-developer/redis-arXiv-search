78acfa06cb27f0d9bbacee14ca42b19ae3f06ad6
Tyler Hutcherson <tyler.hutcherson@redis.com>
2023-10-10 16:40:23 -0400

    docstrings and tweaks to routes

--- backend/vecsim_app/api/routes.py
+++ backend/vecsim_app/api/routes.py
@@ -29,13 +29,14 @@ paper_vector_field_name = "vector"
 
 
 def process_paper(paper) -> t.Dict[str, t.Any]:
-    """_summary_
+    """
+    Process paper data and calculate similarity score.
 
     Args:
-        paper (_type_): _description_
+        paper: Input paper data.
 
     Returns:
-        t.Dict[str, t.Any]: _description_
+        dict: Processed paper data with similarity score.
     """
     if not isinstance(paper, dict):
         paper = paper.__dict__
@@ -45,14 +46,26 @@ def process_paper(paper) -> t.Dict[str, t.Any]:
 
 
 def build_filter_expression(years: list, categories: list) -> FilterExpression:
-    """_summary_
+    """
+    Construct a filter expression based on the provided years and categories.
+
+    This function builds a FilterExpression used for querying papers in Redis.
+    It takes lists of years and categories as input, constructs filter expressions for
+    each set of parameters, and then combines them using the AND operator. The filter
+    expressions for years and categories are constructed by OR'ing individual year and
+    category conditions. If either years or categories list is empty, the function will
+    return a filter expression based solely on the non-empty parameter list.
 
     Args:
-        years (list): _description_
-        categories (list): _description_
+        years (list): A list of years (integers or strings) to be included in the filter
+                      expression. An empty list means there's no filter applied based on years.
+        categories (list): A list of category strings to be included in the filter
+                           expression. An empty list means there's no filter applied based
+                           on categories.
 
     Returns:
-        FilterExpression: _description_
+        FilterExpression: A FilterExpression object representing the combined filter for both years and
+            categories. If both input lists are empty, the function returns None.
     """
     if not years and not categories:
         return None
@@ -79,23 +92,51 @@ def build_filter_expression(years: list, categories: list) -> FilterExpression:
     return year_filter or category_filter
 
 
-def papers_from_results(total: int, results) -> t.Dict[str, t.Any]:
-    """_summary_
+def prepare_response(total: int, results) -> t.Dict[str, t.Any]:
+    """
+    Extract and process papers from search results.
+
+    This function extracts papers from the provided search results, processes each paper,
+    and returns a dictionary containing the total count and a list of processed papers.
 
     Args:
-        total (int): _description_
-        results (_type_): _description_
+        total (int): The hypothetical count of papers present in the db that match the filters.
+        results (list): The iterable containing raw paper data.
 
     Returns:
-        t.Dict[str, t.Any]: _description_
+        dict: A dictionary with 'total' count and a list of 'papers', where each paper is a processed dict.
     """
     # extract papers from VSS results
     if not isinstance(results, list):
         results = results.docs
     return {
         'total': total,
-        'papers': [process_paper(paper) for paper in results.docs]
+        'papers': [process_paper(paper) for paper in results]
     }
+
+
+def create_vector_query(
+    vector: np.ndarray,
+    num_results: int,
+    filter_expression: FilterExpression) -> VectorQuery:
+    """
+    Create and return a VectorQuery instance.
+
+    Args:
+        vector (np.ndarray): The input vector for the query.
+        num_results (int): The number of results to return.
+        filter_expression (FilterExpression): The filter expression for the query.
+
+    Returns:
+        VectorQuery: The constructed VectorQuery instance.
+    """
+    return VectorQuery(
+        vector=vector,
+        vector_field_name=paper_vector_field_name,
+        num_results=num_results,
+        return_fields=["paper_id", "authors", "categories", "year", "title", "vector_distance"],
+        filter_expression=filter_expression
+    )
 
 
 @r.get("/", response_model=t.Dict)
@@ -105,22 +146,22 @@ async def get_papers(
     years: str = "",
     categories: str = ""
 ):
-    """_summary_
+   """Fetch and return papers with optional filtering by years and categories.
 
     Args:
-        limit (int, optional): _description_. Defaults to 20.
-        skip (int, optional): _description_. Defaults to 0.
-        years (str, optional): _description_. Defaults to "".
-        categories (str, optional): _description_. Defaults to "".
+        limit (int, optional): Maximum number of papers to return. Defaults to 20.
+        skip (int, optional): Number of papers to skip for pagination. Defaults to 0.
+        years (str, optional): Comma-separated string of years to filter papers. Defaults to "".
+        categories (str, optional): Comma-separated string of categories to filter papers. Defaults to "".
 
     Returns:
-        _type_: _description_
+        dict: Dictionary containing total count and list of papers.
     """
     query = Query("*")
     # Build query
-    years = [year for year in years.split(",") if year]
-    categories = [cat for cat in categories.split(",") if cat]
-    filter_expression = build_filter_expression(years, categories)
+    year_values = [year for year in years.split(",") if year]
+    category_values = [cat for cat in categories.split(",") if cat]
+    filter_expression = build_filter_expression(year_values, category_values)
     if filter_expression:
         filter_query = FilterQuery(return_fields=[], filter_expression=filter_expression)
         query = filter_query.query
@@ -130,20 +171,21 @@ async def get_papers(
     results = await redis_client.ft(config.DEFAULT_PROVIDER).search(
         query.paging(skip, limit)
     )
-    return papers_from_results(results.total, results)
+    return prepare_response(results.total, results)
 
 
 @r.post("/vectorsearch/paper", response_model=t.Dict)
-async def find_papers_by_text(similarity_request: SimilarityRequest):
-    """_summary_
+async def find_papers_by_paper(similarity_request: SimilarityRequest):
+    """Find and return papers similar to a given paper based on vector similarity.
 
     Args:
-        similarity_request (SimilarityRequest): _description_
+        similarity_request (SimilarityRequest): Similarity request object containing paper_id, provider,
+                                                number_of_results, years, and categories for filtering.
 
     Returns:
-        _type_: _description_
+        dict: Dictionary containing total count and list of similar papers.
     """
-    # Create index
+    # Connect to index
     index_name = similarity_request.provider
     index = await AsyncSearchIndex.from_existing(
         name=index_name,
@@ -163,12 +205,9 @@ async def find_papers_by_text(similarity_request: SimilarityRequest):
     )
 
     # Assemble vector query
-    # TODO need a better way to handle return fields?
-    vector_query = VectorQuery(
+    vector_query = create_vector_query(
         vector=paper_vector,
-        vector_field_name=paper_vector_field_name,
         num_results=similarity_request.number_of_results,
-        return_fields=["paper_id", "authors", "categories", "year", "title", "vector_distance"],
         filter_expression=filter_expression
     )
 
@@ -184,12 +223,20 @@ async def find_papers_by_text(similarity_request: SimilarityRequest):
     )
 
     # Get Paper records of those results
-    # TODO properly post process these results
-    return papers_from_results(count.total, result_papers)
+    return prepare_response(count.total, result_papers)
 
 
 @r.post("/vectorsearch/text", response_model=t.Dict)
-async def find_papers_by_user_text(similarity_request: UserTextSimilarityRequest):
+async def find_papers_by_text(similarity_request: UserTextSimilarityRequest):
+    """Find and return papers similar to user-provided text based on vector similarity.
+
+    Args:
+        similarity_request (UserTextSimilarityRequest): Similarity request object containing user_text, provider,
+                                                        number_of_results, years, and categories for filtering.
+
+    Returns:
+        dict: Dictionary containing total count and list of similar papers.
+    """
     # Attach to index
     index_name = similarity_request.provider
     index = await AsyncSearchIndex.from_existing(
@@ -216,12 +263,9 @@ async def find_papers_by_user_text(similarity_request: UserTextSimilarityRequest
     )
 
     # construct vector query
-    # TODO need a better way to handle return fields?
-    vector_query = VectorQuery(
+    vector_query = create_vector_query(
         vector=query_vector,
-        vector_field_name=paper_vector_field_name,
         num_results=similarity_request.number_of_results,
-        return_fields=["paper_id", "authors", "categories", "year", "title", "vector_distance"],
         filter_expression=filter_expression
     )
 
@@ -231,5 +275,4 @@ async def find_papers_by_user_text(similarity_request: UserTextSimilarityRequest
     )
 
     # Get Paper records of those results
-    # TODO properly post process these results
-    return papers_from_results(count.total, result_papers)
+    return prepare_response(count.total, result_papers)
