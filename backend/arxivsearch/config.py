2cfcd2195c12827cd76db64b23b4da4b85923cf7
Robert Shelton <robert.b.shelton.42@gmail.com>
2024-07-19 14:28:29 -0400

    try localhost

--- backend/arxivsearch/config.py
+++ backend/arxivsearch/config.py
@@ -21,7 +21,7 @@ RETURN_FIELDS = [
 ]
 
 # Redis
-REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
+REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
 if REDIS_PASSWORD:
