cafde63e5020286ef988485e697d0bb52b764566
Robert Shelton <robert.b.shelton.42@gmail.com>
2024-07-25 11:22:59 -0400

    remove unneeded, modify readme, modify test

--- backend/arxivsearch/tests/db/test_load.py
+++ backend/arxivsearch/tests/db/test_load.py
@@ -24,14 +24,21 @@ def test_read_paper_json_local(mock_json_load, mock_file_open, mock_path_join):
 
 
 # Test when the file needs to be fetched from S3
+@patch("arxivsearch.db.load.os.path.isdir")
 @patch("arxivsearch.db.load.os.path.join")
 @patch("arxivsearch.db.load.requests.get")
 @patch("arxivsearch.db.load.open", new_callable=mock_open)
 @patch("arxivsearch.db.load.json.dump")
 @patch("arxivsearch.db.load.json.load", side_effect=Exception("File not found"))
 def test_read_paper_json_s3(
-    mock_json_load, mock_json_dump, mock_file_open, mock_requests_get, mock_path_join
+    mock_json_load,
+    mock_json_dump,
+    mock_file_open,
+    mock_requests_get,
+    mock_path_join,
+    mock_isdir,
 ):
+    mock_isdir.return_value = True
     mock_path_join.return_value = "dummy_path"
     mock_requests_get.return_value.json.return_value = [
         {"id": "5678", "title": "Test Paper from S3"}
